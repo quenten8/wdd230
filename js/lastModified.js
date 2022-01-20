1bc0fae4d17c0de1c5bcb3986cbae85428c7bbbb
quenten8 <quentencox00@byui.edu>
2022-01-20 10:22:48 -0700

    lesson3

--- js/lastModified.js
+++ js/lastModified.js
@@ -5,11 +5,6 @@ function lastModified() {
   let year = get.getUTCFullYear();
   let hour = get.getUTCHours();
   let minute = get.getUTCMinutes();
-  let amOrPm = 'am'
-  if (hour > 12) {
-    amOrPm = 'pm';
-    hour = hour - 12;
-  }
-  date = `${month}/${day}/${year} ${hour}:${minute}${amOrPm}`;
+  date = `${month}/${day}/${year} ${hour}:${minute}`;
   document.getElementById("Modified").innerHTML = date;
 }
