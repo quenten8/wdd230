fe6cbde85da900a6934b26002fd6207b13e334e2
quenten8 <quentencox00@byui.edu>
2022-02-16 21:37:02 -0700

    lesson7

---
+++ chamber/js/lazyload.js
@@ -0,0 +1,22 @@
+function lazyLoad() {
+    const options = {
+        threshold: 0.8,
+        rootMargin: "0px 0px -50px 0px"
+    };
+    document.addEventListener("DOMContentLoaded", function() {
+        const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                const lazyImage = entry.target;
+                lazyImage.src = lazyImage.dataset.src;
+                lazyImage.classList.add("loaded");
+                observer.unobserve(entry.target);
+                }
+            })
+        }, options);
+        const arr = document.querySelectorAll('img.lazy')
+        arr.forEach(function(lazyImage) {
+        observer.observe(lazyImage);
+        })
+    })
+}
