71af9848342b8616f81c314d0ab780b359948af7
quenten8 <quentencox00@byui.edu>
2022-02-17 20:18:07 -0700

    lesson7

---
+++ chamber/js/trackvisit.js
@@ -0,0 +1,23 @@
+function visitDate() {
+    const visited = document.querySelector(".visited");
+    let date = new Date().getTime();
+    let lastVisited = Number(window.localStorage.getItem("visitedDate"));
+    let daysBetween;
+    if(lastVisited == 0)
+    {
+        visited.textContent = "This is your first visit, welcome!";
+    }
+    else
+    {
+      if (lastVisited > 86400000)
+      {
+        visited.textContent = "Welcome back! You last visited earlier today (0 days).";
+      }
+      else
+      {
+        daysBetween = Math.round((date - lastVisited) / (1000 * 3600 * 24));
+        visited.textContent = "Welcome back! You last visited " + daysBetween + " days ago.";
+      }
+    }
+    localStorage.setItem("visitedDate", date);
+}
